89e296ac8b226010f5a9836ae2b2708b26304ecd
Lone Peon <lonepeon@protonmail.com>
2022-10-30 22:26:14 +0100

    Define basic CLI interface

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,33 @@ use clap::Parser;
 
 #[derive(Parser)]
 #[command(author, version, about)]
-struct Cli {}
+struct Cli {
+    #[arg(
+        short = 'd',
+        long = None,
+        default_value=".",
+        env = "STOW_DIR",
+        help = "Set the stow directory instead of using the STOW_DIR environment variable or the current directory"
+    )]
+    source_directory: String,
+    #[arg(
+        short = 't',
+        long = None,
+        env = "HOME",
+        help = "Set the target directory instead of using the HOME folder"
+    )]
+    destination_directory: String,
+    #[arg(help = "All the packages to install on or remove from the system")]
+    packages: Vec<String>,
+}
 
 fn main() {
-    Cli::parse();
+    let cli = Cli::parse();
 
-    println!("Hello, world!");
+    println!(
+        "symlink {} packages from {} onto {}",
+        cli.packages.join(", "),
+        cli.source_directory,
+        cli.destination_directory
+    );
 }
